a367faa591cb8a5e474a3c6ba58a66e74134dec8
Swayamshree Mohanty <swayamshree.appdev@colourmoon.com>
2023-05-27 11:42:02 +0530

    code refactored

--- lib/logic/bloc/counter_bloc.dart
+++ lib/logic/bloc/counter_bloc.dart
@@ -1,7 +1,5 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:meta/meta.dart';
-
 import './counter_state.dart';
 part 'counter_event.dart';
 
